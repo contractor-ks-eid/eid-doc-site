5c08000e8c1e1668ebf67398d76e017ad8669c53
Florin <contractor@ks-eid.com>
2026-08-05 18:46:14 +0300

    Update sanitize-url.rb

--- _plugins/filters/sanitize-url.rb
+++ _plugins/filters/sanitize-url.rb
@@ -1,19 +1,53 @@
 require_relative "../../tools/modules/globals"
+
 require 'uri'
+require 'cgi'
 
 module Jekyll
-    
-    module SanitizeURL
-        def sanitize_url(input)
-            begin
-                uri = URI(input)
-                uri.to_s
-            rescue
-                Globals.putsColText(Globals::YELLOW, "#{input} string is not a valid URL\n")
-            end
-        end
+
+  module SanitizeURL
+    def sanitize_url(input)
+      begin
+        uri = URI(input)
+        uri.to_s
+      rescue URI::InvalidURIError => e
+        Globals.putsColText(
+          Globals::YELLOW,
+          "#{input} is not a valid URL: #{e.message}\n"
+        )
+        input
+      end
     end
+  end
+
+  module EncodeAndSanitizeURL
+    def encode_sanitize_url(input)
+      return input if input.nil? || input.empty?
+
+      begin
+        path, query = input.split('?', 2)
+
+        if query
+          encoded_query = query.split('&').map do |pair|
+            key, value = pair.split('=', 2)
+            "#{key}=#{CGI.escape(value.to_s)}"
+          end.join('&')
+
+          URI("#{path}?#{encoded_query}").to_s
+        else
+          URI(path).to_s
+        end
+      rescue URI::InvalidURIError => e
+        Globals.putsColText(
+          Globals::YELLOW,
+          "#{input} is not a valid URL: #{e.message}\n"
+        )
+        input
+      end
+    end
+  end
 
 end
-  
+
 Liquid::Template.register_filter(Jekyll::SanitizeURL)
+Liquid::Template.register_filter(Jekyll::EncodeAndSanitizeURL)
